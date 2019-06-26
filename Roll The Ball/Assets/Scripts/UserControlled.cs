425adc09b609c1408bb2d3ca6a8a1d8c8acaa2bb
Hemanth <33123731+Hemanth759@users.noreply.github.com>
2019-06-26 18:01:28 +0530

    Initail commit with completed game files

---
+++ Roll The Ball/Assets/Scripts/UserControlled.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UserControlled : MonoBehaviour
+{
+    public float speed;
+    public Text ScoreText;
+    public Text WinText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetScore();
+        WinText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetScore();
+        }
+    }
+
+    void SetScore()
+    {
+        ScoreText.text = "Score: " + count.ToString();
+        if (count >= 14)
+        {
+            WinText.text = "You Win";
+        }
+    }
+}
